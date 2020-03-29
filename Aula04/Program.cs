7afa9f744793102e658cf19aef3ddf1891ba57e7
Álex Junio <alexshox@gmail.com>
2020-03-29 12:44:11 -0300

    Escopo de variável
    E onde  a variavel esta disponivel.

---
+++ Aula04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula04
+{
+    class Program
+    {
+       static int num = 100; // este elemento é  global
+
+        static void Main(string[] args)
+        {
+        //escopod ede ariaveis é como a variável tem area de aatuação
+
+        int num1 = 10; // esste é um exemplo de variável loca
+
+        Console.Write(num);
+
+        }
+        void test() {
+
+            Console.Write(num);
+
+        }
+
+    }
+}
