59f1c1732bddb864ce0935161235d0b6d213fb7a
Álex Junio <alexshox@gmail.com>
2020-03-29 20:19:35 -0300

    Criando constantes
    Muitas vezes precisamsos proteger variaveis de alterlçoes onde se utiliza constantates.

---
+++ Aula07/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           const string texto = "Texto de aprendizado!";  // constantes são obejetos que não podem ter seus valores auterrados.
+           const double pi = 3.141516;
+
+           Console.WriteLine("Imprimmido uma constante: {0} ",pi);
+           
+        }
+    }
+}
