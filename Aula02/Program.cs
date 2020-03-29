c2ac3c6948398f8a1cecfe10d4b11d5a69d7127c
Álex Junio <alexshox@gmail.com>
2020-03-29 11:30:53 -0300

    Aula 02
    nesta auala apredi como inicir um projeto no padrão dotnet utilizando o console comando> DOTNET NEW CONSOLE  e como passar parâmetros para o vertor args  para utilizar no arnnaque no sw.

---
+++ Aula02/Program.cs
@@ -0,0 +1,24 @@
+﻿//Utilizando o comando doctnet new console é possivel criar um projeto mais completo con todas as dependecias dobnet
+// para compilar se utiliza o csc de C Sharp Compile
+//
+
+using System;
+
+namespace Aula02 // padrão de name spaces para niveis de organização como se fosse pacotes em java
+{
+    class Program
+    {
+        static void Main(string[] args) // um padrão de args[] que recebe um argumento em forma de vetor no arranque 
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Isto é uma string vetorizada" [0]); // 
+            
+            //este bloco de código abaixo demonstra como utiliza argumentos em tempo de execução para caso tiver de apassar algo ele ja inicia
+            if (args.GetLength(0) > 0 ){
+                Console.Write(args.GetValue(0));
+            }
+
+
+        }
+    }
+}
