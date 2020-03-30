b705a0239cccf060a99178b1e423e1605bd623cb
Álex Junio <alexshox@gmail.com>
2020-03-29 21:13:24 -0300

    trabalho com biwise

---
+++ Aula09/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //operações de BitWise
+            int num = 4;
+
+            num = num <<2;   // dobra o  dobro
+            //para esquerda crese exponencialmente para direita dimiunui
+            num = num >> 1; // metade
+
+            Console.WriteLine("Bitwise {0}", num);
+        }
+    }
+}
