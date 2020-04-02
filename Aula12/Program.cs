49d352361d61f00305f080bd5c61360485d8690a
Álex Junio <alexshox@gmail.com>
2020-04-01 22:06:37 -0300

    Condicional IF

---
+++ Aula12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        double nota = 0 ;
+        string resultado = "Reprovado" ;
+
+        nota = Double.Parse(Console.ReadLine());
+
+        if ((nota) >= 60){
+            resultado = "Aprovado";
+        }
+
+        Console.WriteLine("O Aluno está: {0}", resultado);
+        }
+    }
+}
